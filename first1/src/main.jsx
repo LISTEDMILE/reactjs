a8d94644b95daed3d69b3f0b9fbbd9a8fddca3e6
LISTEDMILE <listedmile@gmail.com>
2024-09-08 22:04:23 +0530

    GO

--- first1/src/main.jsx
+++ first1/src/main.jsx
@@ -1,9 +1,38 @@
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Print from './own.jsx'
+import React from 'react'
 
+
+/* const Element1 = React.createElement(
+    'a',
+    {
+        href:"#"
+    } ,
+    'click here'
+)
+
+*/
+// aise bhi koi react element create kar skte h
+//par isme sequence bohot matter karega
+
+//type
+//attribute or property
+//child text
+
+
+
+//yha print function import kar liya own.jsx file se....
 createRoot(document.getElementById('root')).render(
-  
+  <>
     <App />
+<Print/>
+
+
+
+    </>
+    
+    //yha print function call kar liya .....
 
 )
---
+++ first1/src/own.jsx
@@ -0,0 +1,24 @@
+//first of all file should be with the name jsx
+
+// function name should be started with a capital latter
+const variable= "this";
+
+//in{} brackets what written is known as expressions
+//outcome of the javascript is used in place of the expression....
+
+function Print(){
+    return(
+        <h1>
+            {variable} is the print statement.....
+        </h1>
+    )
+}
+
+// yha hamne ek function define kiya jo call karne pe run hoga 
+
+// yha ye html components ko export karega
+
+ export default Print
+
+ // iska matlab hamne is file se print function exprot kar diya ......
+ //function ka first letter hmesha capital....
