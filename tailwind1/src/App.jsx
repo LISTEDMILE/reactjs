74a7db51076d1aaa446844b13288c7875fdfc0fc
LISTED MILE <listedmile@gmail.com>
2024-09-11 22:01:58 +0530

    GO

--- tailwind1/src/App.jsx
+++ tailwind1/src/App.jsx
@@ -6,9 +6,26 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  // to insert tailwind inside any tag we can use className and our tailwind properties
+  //tailwind is just like css for styling
+  //first in the terminal write (  npm install -D tailwindcss postcss autoprefixer  )
+  //  then write (  npx tailwindcss init -p  )
+
+  // now in tailwind.config.js replace constant [] to   
+    //  content: [
+    //  "./index.html",
+    //  "./src/**/*.{js,ts,jsx,tsx}",
+    //  ],
+
+    // now in index.css add 
+    //  @tailwind base;
+    //  @tailwind components;
+    //  @tailwind utilities;
+
+    // tailwind is ready to use
   return (
     <>
-     <h1 className="bg-green-400 ">TAILWIND</h1>
+     <h1 className='bg-green-400  rounded-xl text-black p-4'>TAILWIND</h1>
     </>
   )
 }
