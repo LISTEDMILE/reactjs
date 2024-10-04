4d9107ab5e1e5f71c22185cd13081057d5011fb6
LISTED MILE <listedmile@gmail.com>
2024-10-04 22:49:40 +0530

    GO

--- router/src/components/Github/Github.jsx
+++ router/src/components/Github/Github.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useState , useEffect} from "react";
 export default function(){
     const [follow,setFollow] = useState([]);
@@ -6,11 +8,16 @@ export default function(){
         fetch(`https://api.github.com/users/${a}`)
         .then(Response=>Response.json())
         .then(data=>setFollow(data))
-    },[setA]);
+    },[a]);
 
     return(<>
     <div className="flex  justify-center">
-        <input className='m-2 mr-16  bg-slate-300 text-white p-2 text-center' placeholder='git username' onChange={(e)=>{setA(e.target.value)}}/>
-        <div> Followes: {follow.followers}</div></div></>
+        <input className='m-2 mr-16  bg-slate-300 text-black p-2 text-center' 
+        placeholder='git username' 
+        onChange={(e)=>{setA(e.target.value)}}/>
+        <div> Followes: {follow.followers}</div></div>
+        <div className="justify-between ">
+            <img scr={follow.avatar_url} alt="IMG"/>
+            </div></>
     )
 }
