66d45f730101d6b9a9ccc355542a7267bc03f87a
LISTED MILE <listedmile@gmail.com>
2024-10-10 00:25:14 +0530

    GO

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
-
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile.jsx'
+import Login from './components/Login.jsx'
 function App() {
   
+//  useContext is used when we want to access some elements to differents files in a react project. here we first created a file UserContext.js in context folder
+//  then we created a context and stored it in a const and exported it.
+//  then it is imported in UserContextProvider.jsx file in conext folder.
+//  then created a const with UserContextProvider with parameter children .
+//  then created a useState with user and setUser and given it null, then we return (<UserContext.Provider value="user,setUser"> ans {children and closing tag} )
+//  which means that it will be the provider which will be interecting either providing or taking values.
+//  value that will be interecting are user and setUser.
+//  then with useContext imported setUser in Profile.jsx. and altered values of const user.
+//  then simply these values with the help of useContext we have accessed in the Login.jsx......
+
 
   return (
-    <>
-    <h1 className='bg-green-700'>ksdfls</h1>
-    </>
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
   )
 }
 
